7355ad7aa8883d8c0862eb58051abfae7c3c4b0c
Tomohiro Ubukata <ykmr729@gmail.com>
2019-09-22 08:12:13 +0000

    Add Tensor

--- cuxx/dnn/common.h
+++ cuxx/dnn/common.h
@@ -7,6 +7,8 @@
 namespace cuxx {
 namespace dnn {
 
+// TODO(t-ubukata): Consider about copy constructors and move constructors.
+
 class Handle {
  public:
   Handle() {
@@ -17,10 +19,62 @@ class Handle {
     CUXX_DNN_CHECK(cudnnDestroy(raw_handle_));
   }
 
+  Handle(const Handle&) = delete;
+  Handle(const Handle&&) = delete;
+
   cudnnHandle_t raw_handle() {return raw_handle_;}
 
  private:
   cudnnHandle_t raw_handle_;
+};
+
+// The caller must allocate dev_mem previously.
+// TODO(t-ubukata): Consider about type constraints.
+template<typename T>
+class Tensor {
+ public:
+  Tensor(cudnnDataType_t dataType, cudnnTensorFormat_t format,
+         int n, int c, int h, int w, T* dev_mem) : dev_mem_(dev_mem) {
+    CUXX_DNN_CHECK(cudnnCreateTensorDescriptor(&desc_));
+    CUXX_DNN_CHECK(cudnnSetTensor4dDescriptor(desc_, format, dataType,
+                                              n, c, h, w));
+  }
+
+  Tensor(cudnnDataType_t dataType, int n, int c, int h, int w,
+         int nStride, int cStride, int hStride, int wStride, T* dev_mem)
+         : dev_mem_(dev_mem) {
+    CUXX_DNN_CHECK(cudnnCreateTensorDescriptor(&desc_));
+    CUXX_DNN_CHECK(cudnnSetTensor4dDescriptorEx(desc_, dataType,
+                   n, c, h, w, nStride, cStride, hStride, wStride));
+  }
+
+  Tensor(cudnnDataType_t dataType, int nbDims, int dimA[], int strideA[],
+         T* dev_mem) : dev_mem_(dev_mem) {
+    CUXX_DNN_CHECK(cudnnCreateTensorDescriptor(&desc_));
+    CUXX_DNN_CHECK(cudnnSetTensorNdDescriptor(desc_, dataType, nbDims, dimA,
+                                              strideA));
+  }
+
+  Tensor(cudnnTensorFormat_t format, cudnnDataType_t dataType, int nbDims,
+         int dimA[], T* dev_mem) : dev_mem_(dev_mem) {
+    CUXX_DNN_CHECK(cudnnCreateTensorDescriptor(&desc_));
+    CUXX_DNN_CHECK(cudnnSetTensorNdDescriptorEx(desc_, format, dataType, nbDims,
+                                                dimA));
+  }
+
+  Tensor(const Tensor&) = delete;
+  Tensor(const Tensor&&) = delete;
+
+  ~Tensor() {
+    CUXX_DNN_CHECK(cudnnDestroyTensorDescriptor(desc_));
+  }
+
+  cudnnTensorDescriptor_t desc() {return desc;}
+  T* dev_mem() {return dev_mem_;}
+
+ private:
+  cudnnTensorDescriptor_t desc_;
+  T* dev_mem_;
 };
 
 }  // namespace dnn
--- cuxx/dnn/common_test.cc
+++ cuxx/dnn/common_test.cc
@@ -9,13 +9,79 @@ namespace dnn {
 
 class HandleTest : public ::testing::Test {};
 
-TEST_F(HandleTest, Constructor) {
+TEST_F(HandleTest, TestConstructor) {
   Handle handle;
 }
 
-TEST_F(HandleTest, RawHandle) {
+TEST_F(HandleTest, TestRawHandle) {
   Handle handle;
   cudnnHandle_t raw_handle = handle.raw_handle();
+  CUXX_UNUSED_VAR(raw_handle);
+}
+
+class TensorTest : public ::testing::Test {};
+
+TEST_F(TensorTest, TestConstructor4d) {
+  constexpr int n = 2;
+  constexpr int c = 3;
+  constexpr int h = 2;
+  constexpr int w = 2;
+  constexpr int num_elem = n * c * h * w;
+  float* mem_host[num_elem] = {};
+  float* mem_dev = nullptr;
+  cudaMemcpy(mem_host, mem_dev, sizeof(float) * num_elem,
+             cudaMemcpyHostToDevice);
+  Tensor<float> t(CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, n, c, h, w, mem_dev);
+}
+
+TEST_F(TensorTest, TestConstructor4dEx) {
+  constexpr int n = 2;
+  constexpr int c = 3;
+  constexpr int h = 2;
+  constexpr int w = 2;
+  constexpr int num_elem = n * c * h * w;
+  float* mem_host[num_elem] = {};
+  float* mem_dev = nullptr;
+  cudaMemcpy(mem_host, mem_dev, sizeof(float) * num_elem,
+             cudaMemcpyHostToDevice);
+  // NHWC
+  int n_stride = 12;
+  int c_stride = 1;
+  int h_stride = 6;
+  int w_stride = 3;
+  Tensor<float> t(CUDNN_DATA_FLOAT, n, c, h, w,
+                  n_stride, c_stride, h_stride, w_stride, mem_dev);
+}
+
+TEST_F(TensorTest, TestConstructorNd) {
+  constexpr int nbDims = 3;
+  constexpr int n = 4;
+  constexpr int h = 2;
+  constexpr int w = 2;
+  constexpr int num_elem = n * h * w;
+  float* mem_host[num_elem] = {};
+  float* mem_dev = nullptr;
+  cudaMemcpy(mem_host, mem_dev, sizeof(float) * num_elem,
+             cudaMemcpyHostToDevice);
+  // HWN
+  int dimA[nbDims] = {n, h, w};
+  int strideA[nbDims] = {1, 8, 4};
+  Tensor<float> t(CUDNN_DATA_FLOAT, nbDims, dimA, strideA, mem_dev);
+}
+
+TEST_F(TensorTest, TestConstructorNdEx) {
+  constexpr int nbDims = 4;
+  constexpr int n = 2;
+  constexpr int c = 3;
+  constexpr int h = 2;
+  constexpr int w = 2;
+  constexpr int num_elem = n * c * h * w;
+  float* mem_host[num_elem] = {};
+  float* mem_dev = nullptr;
+  cudaMemcpy(mem_host, mem_dev, sizeof(float) * num_elem,
+             cudaMemcpyHostToDevice);
+  int dimA[nbDims] = {n, c, h, w};
+  Tensor<float> t(CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, nbDims, dimA, mem_dev);
 }
 
 }  // namespace dnn

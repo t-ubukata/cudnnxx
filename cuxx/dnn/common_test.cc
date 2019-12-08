9f63470769259facef7984fc6e6452223d931f76
Tomohiro Ubukata <ykmr729@gmail.com>
2019-12-08 02:01:06 +0000

    Some minor fixes

--- cuxx/dnn/common_test.cc
+++ cuxx/dnn/common_test.cc
@@ -23,10 +23,10 @@ TEST_F(TensorTest, TestConstructor4d) {
   constexpr int c = 3;
   constexpr int h = 2;
   constexpr int w = 2;
-  constexpr int num_elem = n * c * h * w;
-  float* mem_host[num_elem] = {};
+  constexpr int n_elem = n * c * h * w;
+  float* mem_host[n_elem] = {};
   float* mem_dev = nullptr;
-  size_t size = sizeof(float) * num_elem;
+  size_t size = sizeof(float) * n_elem;
   cudaMalloc(&mem_dev, size);
   cudaMemcpy(mem_host, mem_dev, size, cudaMemcpyHostToDevice);
   Tensor<float> t(CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, n, c, h, w, mem_dev);
@@ -38,10 +38,10 @@ TEST_F(TensorTest, TestConstructor4dEx) {
   constexpr int c = 3;
   constexpr int h = 2;
   constexpr int w = 2;
-  constexpr int num_elem = n * c * h * w;
-  float* mem_host[num_elem] = {};
+  constexpr int n_elem = n * c * h * w;
+  float* mem_host[n_elem] = {};
   float* mem_dev = nullptr;
-  size_t size = sizeof(float) * num_elem;
+  size_t size = sizeof(float) * n_elem;
   cudaMalloc(&mem_dev, size);
   cudaMemcpy(mem_host, mem_dev, size, cudaMemcpyHostToDevice);
   // NHWC
@@ -55,39 +55,39 @@ TEST_F(TensorTest, TestConstructor4dEx) {
 }
 
 TEST_F(TensorTest, TestConstructorNd) {
-  constexpr int nbDims = 3;
+  constexpr int n_dims = 3;
   constexpr int n = 4;
   constexpr int h = 2;
   constexpr int w = 2;
-  constexpr int num_elem = n * h * w;
-  float* mem_host[num_elem] = {};
+  constexpr int n_elem = n * h * w;
+  float* mem_host[n_elem] = {};
   float* mem_dev = nullptr;
-  size_t size = sizeof(float) * num_elem;
+  size_t size = sizeof(float) * n_elem;
   cudaMalloc(&mem_dev, size);
-  cudaMemcpy(mem_host, mem_dev, sizeof(float) * num_elem,
+  cudaMemcpy(mem_host, mem_dev, sizeof(float) * n_elem,
              cudaMemcpyHostToDevice);
   // HWN
-  int dimA[nbDims] = {n, h, w};
-  int strideA[nbDims] = {1, 8, 4};
-  Tensor<float> t(CUDNN_DATA_FLOAT, nbDims, dimA, strideA, mem_dev);
+  int dims[n_dims] = {n, h, w};
+  int strides[n_dims] = {1, 8, 4};
+  Tensor<float> t(CUDNN_DATA_FLOAT, n_dims, dims, strides, mem_dev);
   cudaFree(mem_dev);
 }
 
 TEST_F(TensorTest, TestConstructorNdEx) {
-  constexpr int nbDims = 4;
+  constexpr int n_dims = 4;
   constexpr int n = 2;
   constexpr int c = 3;
   constexpr int h = 2;
   constexpr int w = 2;
-  constexpr int num_elem = n * c * h * w;
-  float* mem_host[num_elem] = {};
+  constexpr int n_elem = n * c * h * w;
+  float* mem_host[n_elem] = {};
   float* mem_dev = nullptr;
-  size_t size = sizeof(float) * num_elem;
+  size_t size = sizeof(float) * n_elem;
   cudaMalloc(&mem_dev, size);
-  cudaMemcpy(mem_host, mem_dev, sizeof(float) * num_elem,
+  cudaMemcpy(mem_host, mem_dev, sizeof(float) * n_elem,
              cudaMemcpyHostToDevice);
-  int dimA[nbDims] = {n, c, h, w};
-  Tensor<float> t(CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, nbDims, dimA, mem_dev);
+  int dims[n_dims] = {n, c, h, w};
+  Tensor<float> t(CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, n_dims, dims, mem_dev);
   cudaFree(mem_dev);
 }
 
--- cuxx/dnn/op_tensor_test.cc
+++ cuxx/dnn/op_tensor_test.cc
@@ -26,10 +26,10 @@ TEST_F(OpTensorTest, TestAdd) {
   constexpr int c = 3;
   constexpr int h = 2;
   constexpr int w = 2;
-  constexpr int num_elem = n * c * h * w;
-  size_t size = sizeof(float) * num_elem;
+  constexpr int n_elem = n * c * h * w;
+  size_t size = sizeof(float) * n_elem;
 
-  float a_host[num_elem] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6,
+  float a_host[n_elem] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6,
                             0.7, 0.8, 0.9, 1.0, 1.1, 1.2,
                             1.3, 1.4, 1.5, 1.6, 1.7, 1.8,
                             1.9, 2.0, 2.1, 2.2, 2.3, 2.4};
@@ -39,7 +39,7 @@ TEST_F(OpTensorTest, TestAdd) {
   Tensor<float> a_tensor(CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, n, c, h, w,
                          a_dev);
 
-  float b_host[num_elem] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6,
+  float b_host[n_elem] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6,
                             0.7, 0.8, 0.9, 1.0, 1.1, 1.2,
                             1.3, 1.4, 1.5, 1.6, 1.7, 1.8,
                             1.9, 2.0, 2.1, 2.2, 2.3, 2.4};
@@ -49,7 +49,7 @@ TEST_F(OpTensorTest, TestAdd) {
   Tensor<float> b_tensor(CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, n, c, h, w,
                          b_dev);
 
-  float c_host[num_elem] = {};
+  float c_host[n_elem] = {};
   float* c_dev;
   cudaMalloc(&c_dev, size);
   cudaMemcpy(c_dev, c_host, size, cudaMemcpyHostToDevice);
@@ -62,11 +62,11 @@ TEST_F(OpTensorTest, TestAdd) {
   cudaDeviceSynchronize();
   cudaMemcpy(c_host, c_dev, size, cudaMemcpyDeviceToHost);
 
-  float c_expected[num_elem] = {0.2, 0.4, 0.6, 0.8, 1.0, 1.2,
+  float c_expected[n_elem] = {0.2, 0.4, 0.6, 0.8, 1.0, 1.2,
                                 1.4, 1.6, 1.8, 2.0, 2.2, 2.4,
                                 2.6, 2.8, 3.0, 3.2, 3.4, 3.6,
                                 3.8, 4.0, 4.2, 4.4, 4.6, 4.8};
-  for (auto i = 0; i < num_elem; ++i) {
+  for (int i = 0; i < n_elem; ++i) {
     EXPECT_NEAR(c_expected[i], c_host[i], 1e-4) << "i: " << i;
   }
   cudaFree(c_dev);

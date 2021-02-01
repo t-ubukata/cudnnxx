188e72736c6ae07084087d2fbb16828b8a17c3f0
Tomohiro Ubukata <ykmr729@gmail.com>
2021-02-01 22:17:24 +0900

    Add CTCLoss::GetWorkspaceSize

--- cudnnxx/ctc_loss.h
+++ cudnnxx/ctc_loss.h
@@ -1,6 +1,8 @@
 #ifndef CUDNNXX_CTC_LOSS_H_
 #define CUDNNXX_CTC_LOSS_H_
 
+#include <vector>
+
 #include "cudnn.h"
 #include "cudnnxx/common.h"
 #include "cudnnxx/util.h"
@@ -18,21 +20,30 @@ class CTCLoss {
   CTCLoss(cudnnDataType_t comp_type, cudnnLossNormalizationMode_t norm_mode,
           cudnnNanPropagation_t grad_mode) {
     CUDNNXX_DNN_CHECK(cudnnCreateCTCLossDescriptor(&desc_));
-    CUDNNXX_DNN_CHECK(cudnnSetCTCLossDescriptorEx(desc_, comp_type, norm_mode,
-                                                grad_mode));
+    CUDNNXX_DNN_CHECK(
+        cudnnSetCTCLossDescriptorEx(desc_, comp_type, norm_mode, grad_mode));
   }
 
-  ~CTCLoss() {
-    CUDNNXX_DNN_CHECK(cudnnDestroyCTCLossDescriptor(desc_));
-  }
+  ~CTCLoss() { CUDNNXX_DNN_CHECK(cudnnDestroyCTCLossDescriptor(desc_)); }
 
   CTCLoss(const CTCLoss&) = delete;
   CTCLoss operator=(const CTCLoss&) = delete;
 
   cudnnCTCLossDescriptor_t desc() const { return desc_; }
 
+  size_t GetWorkspaceSize(const Handle& handle, const Tensor<TensorT>& probs,
+                          const Tensor<TensorT>& gradients, int* labels,
+                          int* label_lengths, int* input_lengths,
+                          cudnnCTCLossAlgo_t algo) {
+    size_t size_in_bytes = 0;
+    // Note: labels is host memory and ideally an tensor.
+    CUDNNXX_DNN_CHECK(cudnnGetCTCLossWorkspaceSize(
+        handle.raw_handle(), probs.desc(), gradients.desc(), labels,
+        label_lengths, input_lengths, algo, desc_, &size_in_bytes));
+    return size_in_bytes;
+  }
+
   // cudnnCTCLoss()
-  // cudnnGetCTCLossWorkspaceSize()
 
  private:
   cudnnCTCLossDescriptor_t desc_;
